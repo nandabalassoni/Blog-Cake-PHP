5956b3f1c7defdd3bb2f94b1770e87ccabde1623
Maria <mariafernandabalassoni@hotmail.com>
2020-01-17 16:58:23 -0300

    Criando Controller para Posts

--- www/app/Controller/PostsController.php
+++ www/app/Controller/PostsController.php
@@ -2,5 +2,13 @@
 class PostsController extends AppController {
     public $helpers = array ('Html','Form');
     public $name = 'Posts';
+
+    function index() {
+        $this->set('posts', $this->Post->find('all'));
+    }
+
+    public function view($id = null) {
+        $this->set('post', $this->Post->findById($id));
+    }
 }
 ?>
